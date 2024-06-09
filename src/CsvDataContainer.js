d3195c3b5bbdcd2fc1d0d23929be7c96cdbe8f9e
lic <lic_cake@yahoo.co.jp>
2024-06-10 07:58:02 +0900

    メソッドの列挙、追加

--- src/CsvDataContainer.js
+++ src/CsvDataContainer.js
@@ -2,106 +2,262 @@
  * CSVを操作するクラス。
  * CsvDataContainer
  * 
- * CsvDataContainer(String csvData, Boolean isRecordBased, String | undefined primaryKey)
+ * CsvDataContainer(String csvData, String | undefined primaryKey)
  * csvDataをCSVデータと解釈し、パースしてObjectに変換します。
- * isRecordBasedは変換形式を表します。
- * isRecordBasedがtrueの場合は以下の形式に変換して格納します。
- * {
- *  0: [FieldName1, FieldName2, FieldName3, ...],
- *  1: [Record1-1, Record1-2, Record1-3, ...],
- *  2: [Record2-1, Record2-2, Record2-3, ...],
- *  3: [Record3-1, Record3-2, Record3-3, ...]
- *  ...
- * }
- * 
- * isRecordBasedがfalseの場合は以下の形式に変換して格納します。
- * {
- *  0: [FieldName1, Record1-1, Record2-1, Record3-1, ...],
- *  1: [FieldName2, Record1-2, Record2-2, Record3-2, ...],
- *  2: [FieldName3, Record1-3, Record2-3, Record3-3, ...],
- *  ...
- * }
  * 
  * クラスのフィールド
- * isRecordBased: 変換形式
- * primaryKey: 主キー
- * csvObj: CSVデータ
+ * #primaryKey: 主キー
+ * #csvObj: CSVデータ
  * 
- * メソッド
- * getValueByRecordIndex(String fieldName, Number recordIndex): String
- * getValueByPrimaryKey(String fieldName, String primaryKey): String
+ * ================================
+ * private method
+ * ================================
+ * #convertCsvToObj(String csvData): Array[{record1}, {record2}, ...]
+ * #convertObjToCsv(Array[{record1}, {record2}, ...]): String
  * 
- * setValueByRecordIndex(String fieldName, Number recordIndex, String value): void
- * setValueByPrimaryKey(String fieldName, String primaryKey, String value): void
+ * ================================
+ * public method
+ * ================================
+ * addRecord(Object record): void
  * 
- * setPrimaryKey(String primaryKey): void
- * setIsRecordBased(Boolean isRecordBased): void
+ * --------------------------------
+ * deleteRecordByPrimaryKey(String primaryKey): void
+ * deleteRecordByRecordIndex(Number recordIndex): void
  * 
+ * deleteMultipleRecordsByPrimaryKey(String primaryKey...): void
+ * deleteMultipleRecordsByRecordIndex(Number recordIndex...): void
+ * 
+ * --------------------------------
+ * searchDuplicatePrimaryKey(): Array[String]
+ * searchDuplicateValueByFieldName(String fieldName): Array[String]
+ * searchDuplicateValue(String fieldName, String value): Array[String]
+ * 
+ * --------------------------------
+ * checkDuplicatePrimaryKey(): Boolean
+ * checkDuplicateValueByFieldName(String fieldName): Boolean
+ * checkDuplicateValue(String fieldName, String value): Boolean
+ * 
+ * --------------------------------
+ * 正規表現を使用して判定する。
+ * checkIllegalValueByFieldName(String fieldName, String legalValue): Boolean
+ * checkIllegalValue(String fieldName, String value, String legalValue): Boolean
+ * 
+ * --------------------------------
  * getRecordCount(): Number
  * getFieldCount(): Number
  * 
+ * --------------------------------
+ * getCsvObj(): Object
  * getCsvData(): String
  * 
- * formatRecordBased(String csvData): void
- * formatFieldBased(String csvData): void
+ * ------------------------------
+ * getRecordByPrimaryKey(String primaryKey): Object
+ * getRecordByRecordIndex(Number recordIndex): Object
+ * getValueByFieldName(String fieldName): Array[String]
+ * 
+ * getMultipleRecordsByPrimaryKey(String primaryKey...): Array[{Object}, {Object}, ...]
+ * getMultipleRecordsByRecordIndex(Number recordIndex...): Array[{Object}, {Object}, ...]
+ * getMultipleValuesByFieldName(String fieldName...): Array[Array[String], Array[String], ...]
+ * 
+ * --------------------------------
+ * getValueByRecordIndex(String fieldName, Number recordIndex): String
+ * getValueByPrimaryKey(String fieldName, String primaryKey): String
+ * 
+ * --------------------------------
+ * setValueByRecordIndex(String fieldName, Number recordIndex, String value): void
+ * setValueByPrimaryKey(String fieldName, String primaryKey, String value): void
+ * 
+ * --------------------------------
+ * setPrimaryKey(String primaryKey): void
+ */
+/**
+ * レコードを参照するメソッドは一つのメソッドではなく主キーとレコード番号に分かれている。
+ * もし一つのメソッドで実装しようとすると引数で主キーorレコード番号を判断することになるが、それを判断するために条件分岐の実装が必要である。
+ * もしそう実装した場合、膨大な量のレコード参照を行う際毎回条件分岐が走ることになり、パフォーマンスに支障が出る。
  */
 
 export class CsvDataContainer {
   /**
+   * @type {String}
+   */
+  #primaryKey;
+  /**
+   * @type {Object}
+   */
+  #csvObj;
+
+  /**
    * CSVデータを操作するクラス。
    * @param {String} csvData CSVデータ
-   * @param {Boolean} isRecordBased 変換形式
+   * @param {String | undefined} primaryKey 主キー
+   */
+  constructor(csvData, primaryKey) {
+    this.#primaryKey = primaryKey;
+    this.#csvObj = this.#convertCsvToObj(csvData);
+  }
+
+  /**
+   * CSVをObjectに変換する
+   * @param {String} csvData
+   * @returns {Array[{record1}, {record2}, ...]} csvObj
+   */
+  #convertCsvToObj(csvData) {
+    const lines = csvData.split('\n');
+    const headers = lines[0].split(',');
+    return lines.slice(1).map(line => {
+      const data = line.split(',');
+      return headers.reduce((obj, header, index) => {
+        obj[header] = data[index];
+        return obj;
+      }, {});
+    });
+  }
+
+  /**
+   * ObjectをCSVに変換する
+   * @param {Array[{record1}, {record2}, ...]} csvObj 
+   * @returns {String} CSVデータ
+   */
+  #convertObjToCsv(csvObj) {
+    const headers = Object.keys(csvObj[0]);
+    const lines = csvObj.map(record => headers.map(header => record[header]).join(','));
+    return `${headers.join(',')}\n${lines.join('\n')}`;
+  }
+
+  /**
+   * レコードを追加する
+   * @param {Object} record
+   */
+  addRecord(record) {
+    if(record[this.#primaryKey] === undefined) {
+      throw new Error(`主キー${this.#primaryKey}が設定されていないレコードは追加できません。`);
+    }
+    this.#csvObj.push(record);
+  }
+
+  //getter
+  /**
+   * レコードの値を取得する(レコード番号で取得)
+   * @param {String} fieldName フィールド名
+   * @param {Number} recordIndex レコード番号
+   * @returns {String} レコードの値
+   */
+  getValueByRecordIndex(fieldName, recordIndex) {
+    return this.#csvObj[recordIndex][fieldName];
+  }
+
+  /**
+   * レコードの値を取得する(主キーで取得)
+   * @param {String} fieldName フィールド名
+   * @param {String} primaryKey 主キー
+   * @returns {String} レコードの値
+   */
+  getValueByPrimaryKey(fieldName, primaryKey) {
+    return this.#csvObj.find(record => record[this.#primaryKey] === primaryKey)[fieldName];
+  }
+
+  /**
+   * レコード総数を取得する
+   * @returns {Number} レコード総数
+   */
+  getRecordCount() {
+    return this.#csvObj.length;
+  }
+
+  /**
+   * フィールド総数を取得する
+   * @returns {Number} フィールド総数
+   */
+  getFieldCount() {
+    return Object.keys(this.#csvObj[0]).length;
+  }
+
+  /**
+   * csvObjを取得する
+   * @returns {Object} csvObj
+   */
+  getCsvObj() {
+    return this.#csvObj;
+  }
+
+  /**
+   * CSVデータを取得する
+   * @returns {String} CSVデータ
+   */
+  getCsvData() {
+    return this.#convertObjToCsv(this.#csvObj);
+  }
+
+  /**
+   * レコードを取得する(主キーで取得)
+   * @param {String} primaryKey 主キー
+   * @returns {Object} レコード
+   */
+  getRecordByPrimaryKey(primaryKey) {
+    return this.#csvObj.find(record => record[this.#primaryKey] === primaryKey);
+  }
+
+  /**
+   * レコードを取得する(レコード番号で取得)
+   * @param {Number} recordIndex レコード番号
+   * @returns {Object} レコード
+   */
+  getRecordByRecordIndex(recordIndex) {
+    return this.#csvObj[recordIndex];
+  }
+
+  /**
+   * フィールドの値を取得する
+   * @param {String} fieldName フィールド名
+   * @returns {Array[String]} フィールドの値
+   */
+  getValueByFieldName(fieldName) {
+    return this.#csvObj.map(record => record[fieldName]);
+  }
+
+  //setter
+  /**
+   * レコードの値を設定する(レコード番号で設定)
+   * @param {String} fieldName フィールド名
+   * @param {Number} recordIndex レコード番号
+   * @param {String} value レコードの値
+   */
+  setValueByRecordIndex(fieldName, recordIndex, value) {
+    this.#csvObj[recordIndex][fieldName] = value;
+  }
+
+  /**
+   * レコードの値を設定する(主キーで設定)
+   * @param {String} fieldName フィールド名
+   * @param {String} primaryKey 主キー
+   * @param {String} value レコードの値
+   */
+  setValueByPrimaryKey(fieldName, primaryKey, value) {
+    this.#csvObj.find(record => record[this.#primaryKey] === primaryKey)[fieldName] = value;
+  }
+
+  /**
+   * 主キーを設定する
    * @param {String} primaryKey 主キー
    */
-  constructor(csvData, isRecordBased, primaryKey) {
+  setPrimaryKey(primaryKey) {
+    this.#primaryKey = primaryKey;
   }
 
   /**
-   * CSVデータをRecordをベースにした形式に変換します。
-   * csvObj{
-   *  0: [FieldName1, FieldName2, FieldName3, ...],
-   *  1: [Record1-1, Record1-2, Record1-3, ...],
-   *  2: [Record2-1, Record2-2, Record2-3, ...],
-   *  3: [Record3-1, Record3-2, Record3-3, ...]
-   *  ...
-   * }
-   * @param {String | undefined} csvData CSVデータ
-   * @returns {CsvDataContainer} CSVデータをRecordをベースにした形式に変換したクラス
+   * レコードを削除する(主キーで削除)
+   * @param {String} primaryKey 主キー
    */
-  formatRecordBased(csvData = this.csvObj) {
-    class CsvRecordBased extends CsvDataContainer {
-      constructor(csvData, primaryKey) {
-        super(csvData, true, primaryKey);
-        this.csvObj = this.formatRecordBased(csvData);
-      }
-
-      formatRecordBased(csvData) {
-        const lines = csvData.split('\n');
-        const headers = lines[0].split(',');
-        return lines.slice(1).map(line => {
-          const data = line.split(',');
-          return headers.reduce((obj, header, index) => {
-            obj[header] = data[index];
-            return obj;
-          }, {});
-        });
-      }
-    }
-    return new CsvRecordBased(csvData, primaryKey);
+  deleteRecordByPrimaryKey(primaryKey) {
+    this.#csvObj = this.#csvObj.filter(record => record[this.#primaryKey] !== primaryKey);
   }
 
   /**
-   * CSVデータをFieldをベースにした形式に変換します。
-   * csvObj{
-   *  0: [FieldName1, Record1-1, Record2-1, Record3-1, ...],
-   *  1: [FieldName2, Record1-2, Record2-2, Record3-2, ...],
-   *  2: [FieldName3, Record1-3, Record2-3, Record3-3, ...],
-   *  ...
-   * }
-   * @param {String} csvData CSVデータ
+   * レコードを削除する()
+   * @param {Number} recordIndex レコード番号
    */
-  formatFieldBased(csvData) {
-    
+  deleteRecordByRecordIndex(recordIndex) {
+    this.#csvObj.splice(recordIndex, 1);
   }
 }
 
